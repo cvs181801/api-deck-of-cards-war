a77759b3a60c7748b5cb1a0ff65bd24ebc658f7a
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-09-14 10:45:52 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+//grab elements from the DOM
+dealButton = document.querySelector(".btn__deal");
+
+//create a way to get a new deck of cards from the deck of cards api
+dealButton.addEventListener("click", function(e) {
+fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+    .then(res=> res.json())
+    .then((data) => {
+        console.log(data)
+    });
+})
