90c1ce7ab2232509597ae1cce6361af04c49232f
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-09-14 15:38:03 -0700

    add in a way to draw 2 cards and enable jokers

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 //grab elements from the DOM
 dealButton = document.querySelector(".btn__deal");
+drawButton = document.querySelector(".btn__draw");
 
 //create a way to get a new deck of cards from the deck of cards api.
 
 dealButton.addEventListener("click", function(e) {
     e.preventDefault();
-fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
-    .then(res=> res.json())
+    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/draw/?count=2", {
+        method: 'GET',
+        jokers_enabled = true,
+    })
+    .then(res => res.json())
     .then((data) => {
-        const deckId = `${data.deck_id}`;
-        console.log(deckId);
+        let deckId = `${data.deck_id}`;
+        console.log(data);
+        //console.log(`https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`);
+       
     });
+
+    //drawButton.addEventListener("click", function(e) {
+        //e.preventDefault();
+        //console.log(`https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`);
+    //    fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
+    //     .then(res => res.json())
+    //     .then((data) => {
+    //         console.log(data); 
+    //     });
+    // })
+
 })
 
