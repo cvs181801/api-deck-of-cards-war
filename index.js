eaac74198a2dd5b94ab4ddb0ce3497b1f1838392
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-09-14 14:58:32 -0700

    save the deck id to a variable

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 //grab elements from the DOM
 dealButton = document.querySelector(".btn__deal");
 
-//create a way to get a new deck of cards from the deck of cards api
+//create a way to get a new deck of cards from the deck of cards api.
+
 dealButton.addEventListener("click", function(e) {
     e.preventDefault();
 fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
     .then(res=> res.json())
     .then((data) => {
-        console.log(data)
+        const deckId = `${data.deck_id}`;
+        console.log(deckId);
     });
 })
+
