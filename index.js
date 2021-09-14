14e1bc6b21ac4eb45f142deb77e3a0f530f2f232
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-09-14 15:53:51 -0700

    add a way to draw two cards and render card imgages on screen

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //grab elements from the DOM
-dealButton = document.querySelector(".btn__deal");
-drawButton = document.querySelector(".btn__draw");
+const dealButton = document.querySelector(".btn__deal");
+const container = document.querySelector(".container");
 
 //create a way to get a new deck of cards from the deck of cards api.
 
@@ -8,25 +8,20 @@ dealButton.addEventListener("click", function(e) {
     e.preventDefault();
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/draw/?count=2", {
         method: 'GET',
-        jokers_enabled = true,
+        jokers_enabled: true,
     })
     .then(res => res.json())
     .then((data) => {
         let deckId = `${data.deck_id}`;
         console.log(data);
-        //console.log(`https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`);
-       
+        let card1Img = document.createElement('img');
+        card1Img.src = `${data.cards[0].image}`
+        container.append(card1Img);
+        let card2Img = document.createElement('img');
+        console.log(`${data.cards[1].image}`)
+        card2Img.src = `${data.cards[1].image}`;
+        container.append(card2Img);
     });
-
-    //drawButton.addEventListener("click", function(e) {
-        //e.preventDefault();
-        //console.log(`https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`);
-    //    fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
-    //     .then(res => res.json())
-    //     .then((data) => {
-    //         console.log(data); 
-    //     });
-    // })
 
 })
 
