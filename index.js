c192c36a1d06889bcf3dae99f400966408304cae
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-09-14 11:05:17 -0700

    add button background color and prevent default to e listener

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ dealButton = document.querySelector(".btn__deal");
 
 //create a way to get a new deck of cards from the deck of cards api
 dealButton.addEventListener("click", function(e) {
+    e.preventDefault();
 fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
     .then(res=> res.json())
     .then((data) => {
