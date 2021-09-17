2e6d15860e42166ef74475e6ab96422ebaeed8f1
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-09-16 17:17:47 -0700

    add a basic algorithm to compare the computer's hand w the users

--- index.js
+++ index.js
@@ -21,6 +21,51 @@ dealButton.addEventListener("click", function(e) {
         console.log(`${data.cards[1].image}`)
         card2Img.src = `${data.cards[1].image}`;
         container.append(card2Img);
+
+        let playerCardValue = data.cards[0].value;
+        let computerCardValue = data.cards[1].value;
+
+        if (playerCardValue == "ACE") {
+            playerCardValue = 14;
+        } else if (playerCardValue == "KING") {
+            playerCardValue = 13;
+        } else if (playerCardValue == "QUEEN") {
+            playerCardValue = 12;
+        } else if (playerCardValue == "JACK") {
+            playerCardValue = 11;
+        } else if (playerCardValue == "10" || "9" || "8" || "7" || "6" || "5" || "4" || "3" || "2") {
+            parseInt(playerCardValue);
+        }
+
+        console.log(playerCardValue);
+
+
+        if (computerCardValue == "ACE") {
+            computerCardValue = 14;
+        } else if (computerCardValue == "KING") {
+            computerCardValue = 13;
+        } else if (computerCardValue == "QUEEN") {
+            computerCardValue = 12;
+        } else if (computerCardValue == "JACK") {
+            computerCardValue = 11;
+        } else if (computerCardValue == "10" || "9" || "8" || "7" || "6" || "5" || "4" || "3" || "2") {
+            parseInt(computerCardValue);
+        }
+
+        console.log(computerCardValue);
+
+        function compareHands(card1, card2) {
+
+        if (playerCardValue == computerCardValue) {
+            console.log("its a tie");
+        } else if (playerCardValue > computerCardValue) {
+            console.log("player 1 wins");
+        } else {
+            console.log("computer wins");
+        }
+    }
+        compareHands(playerCardValue, computerCardValue);
+
     });
 
 })
